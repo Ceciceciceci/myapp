e82ed0ec6068f628045b17404f9d3602b90e4270
Ceci Tran <ceci@Cecis-MacBook-Pro.local>
2020-06-17 21:27:02 -0700

    update to text and responsiveness

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -19,18 +19,21 @@ class Resume extends Component{
                         </div>
 
                         <div className="job-list">
+                            <h2 className="job"><b>Work Experience</b></h2>
                             <div className="job">
                                  <h3>Classic Vacations / Expedia Groups : Web Developer &amp;&amp; UI/UX Engineer</h3>
                                 <p className="date">Jan 2019 to CURRENT</p>
                                 <p className="job-desc">
                                 Javascript , Drupal, CSS3, HTML5, PHP, MySQL, React 
                                 <br />
-                                <li>Created and optimized Javascript and PHP code for different pages and designs for the Classic Vacations site. </li>
-                                <li>Wireframed and worked with the UX designer and the developer team in India to successfully build a better usable website and optimize the design code. </li>
-                                <li>Created a style guide with the UX designer to help organize the site's new style and brand.</li>
-                                <li>Self learned as the only engineer on the team for the first 4 months, a new language and infrastructure, Drupal and php. Introduced React to the team for possibility for changing frameworks to better optimize the site.</li>
-                                <li>Minimized the amount of libraries imported with CDNs needed for design prototypes into a few lines of Javascript.  Made the prototypes clean and robust enough for use in development. </li>
-                                <li>Created new documentation for any existing and new code worked on and organized it into documents and onto JIRA boards as the old code didn't have prior documentation to explain certain features.</li>
+                                <ul>
+                                    <li>Created and optimized Javascript and PHP code for different pages and designs for the Classic Vacations site. </li>
+                                    <li>Wireframed and worked with the UX designer and the developer team in India to successfully build a better usable website and optimize the design code. </li>
+                                    <li>Created a style guide with the UX designer to help organize the site's new style and brand.</li>
+                                    <li>Self learned as the only engineer on the team for the first 4 months, a new language and infrastructure, Drupal and php. Introduced React to the team for possibility for changing frameworks to better optimize the site.</li>
+                                    <li>Minimized the amount of libraries imported with CDNs needed for design prototypes into a few lines of Javascript.  Made the prototypes clean and robust enough for use in development. </li>
+                                    <li>Created new documentation for any existing and new code worked on and organized it into documents and onto JIRA boards as the old code didn't have prior documentation to explain certain features.</li>
+                                </ul>
                                 </p>
                             </div>
                             <div className="job">
@@ -39,10 +42,12 @@ class Resume extends Component{
                                 <p className="job-desc">
                                 HTML5, CSS, Javascript, Perl (backend), Vue.js, Catalyst, Distributed systems, Basalmiq
                                 <br />
+                                <ul>
                                 <li>Created frontend features and wireframes for a distributed network cluster system application A3.</li>
                                 <li>Introduced and integrated Vue.js library into project front end framework change from Catalyst.</li>
                                 <li>Communicated with overseas team in China to discuss tasks and complete them</li>
                                 <li>Redesigned and drew up wireframes for part of the A3 application to help fix current issues in the displaying new information and features and then develop the features frontend and backend with backend engineers.</li>
+                                </ul>
                                 </p>
                             </div>
                             <div className="job">
@@ -50,11 +55,13 @@ class Resume extends Component{
                                 <p className="date">Jun 2017 to Jan 2018</p>
                                 <p className="job-desc">SAS, HTML5, CSS, JS, Google Charts API, JQuery, UI/UX wireframing, Python, Basalmiq
                                 <br />
+                                <ul>
                                 <li>Successfully updated/modified the Disney Theatrical System with SAS, Javascript, CSS3, Google Charts API, JQuery</li>
                                 <li>Created and wireframed a new UX/UI flow for custom query selection for a generated proposal application for using Basalmiq and Powerpoint</li>
                                 <li>Multitasked and redid documentation on multiple projects at a time to develop new client needs and write tests using Python, Javascript</li>
                                 <li>Modified layouts and code to improve documentation and flow of the website.</li>
                                 <li>Wireframed flow for future query function application.</li>
+                                </ul>
                                 </p>
                             </div>
                             <div className="job">
@@ -63,9 +70,11 @@ class Resume extends Component{
                                 <p className="job-desc">
                                 Python, JS, HTML
                                 <br />
+                                <ul>
                                 <li>Wrote new scripts to help further improve the QA team’s ability to diagnose hardware components in routers/chassis from outside of the lab and to test untested components. Used TMUX, Linux, and TCL to write scripts.</li>
                                 <li>Tested hardware and scripts in the server lab.</li>
                                 <li>Improved the QA website by adding small helpful features such as recognizing who is running a test at a certain time.</li>
+                                </ul>
                                 </p>
                             </div> 
                         </div>
