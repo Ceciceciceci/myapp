68bcdc113310a4444d5daa7b30b8d4a2ec49e01b
Cecilia Tran <cecitran@cecilias-mbp.lan>
2021-02-13 00:30:33 -0800

    updates to info and layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,15 +25,7 @@ class Header extends Component{
                         <ul className="nav-links" key={navItem.id}>
                             <li className="home-link" onClick={this.closeMenu}><Link to="/">home</Link></li>
                             <li><Link className="logo" to="/" onClick={this.closeMenu}>
-                                    <svg width="120" height="108" viewBox="0 0 120 108" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="120" height="108">
-                                        <ellipse cx="60" cy="54" rx="60" ry="54" fill="#C46D24"/>
-                                        </mask>
-                                        <g mask="url(#mask0)">
-                                        <ellipse cx="60" cy="54" rx="60" ry="54" fill="#C46D24"/>
-                                        </g>
-                                        <path d="M35 16C33 11.6 34 8.99999 35 8C36 7 37.7 7 40.5 7C44 7 50 10.5 53 14C55.4 16.8 57 18.5 58 20C59 18.5 62.9 15 74.5 11C89 6.00001 84 12.5 83 14.5C82 16.5 78.5001 19.9999 77.0001 21.4999L77 21.5C75.9802 22.5198 71.8333 24.8333 69.5 26C70 26.5 68.5 25.6 74.5 30C82 35.5 89.5 45.5 84.5 49C79.5 52.5 71.5 49.5 65.5 46.5C60.7 44.1 56.1667 40.5 54.5 39C52 41.5 41 45.5 37 46.5C33 47.5 23 48 21.5 46.5C20 45 21 42 24 38C26.4 34.8 32.3333 31.3333 35 30L42.5 27.5C41 26 37.5 21.5 35 16Z" fill="#FFF3D6"/>
-                                    </svg>
+                                    home
                                 </Link>
                             </li>
                             <li><a href="/#about" onClick={this.closeMenu}>about </a></li>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,74 @@
 import React, { Component } from 'react';
 
 class Resume extends Component{
+    state = {
+        jobs : [
+            { 
+                company: 'Future Motion', 
+                title: 'Software Engineer', 
+                date:'Oct 2020 to Dec 2020',
+                location: 'Santa Cruz, CA',
+                tools: 'Javascript, CSS3, HTML5, Python, React',
+                jobDesc: [
+                    "Created front-end features for an internal tool on web to speed up factory picking, packing, and shipping process of EV skateboards.",
+                ]
+            },
+            { 
+                company: 'Classic Vacations', 
+                title: 'Web Developer', 
+                date:'Jan 2019 to Sept 2020', 
+                location: 'San Jose, CA',
+                tools: 'Javascript, Drupal, CSS3, HTML5, PHP, MySQL, React',
+                jobDesc: [
+                    "Wireframed and worked with the UX designer and the developer team in India to successfully build and deploy, and optimize a new site redesign.",
+                    "Created a style guide with the UX designer to help organize the site's new style and brand.",
+                    "Self learned as the only engineer on the team for the first 4 months, a new language and infrastructure, Drupal and PHP. Introduced React to the team for possibility for changing frameworks to better optimize the site.",
+                    "Minimized the amount of libraries imported with CDNs needed into a few lines of Javascript.",
+                    "Created new documentation for any existing and new code worked on and organized it into documents and onto JIRA boards as the old code didn't have prior documentation to explain certain features."
+                ]
+            },
+            { 
+                company: 'Aerohive Networks', 
+                title: 'Fullstack Engineer', 
+                date:'Jan 2018 to Dec 2019', 
+                location: 'Milpitas, CA',
+                tools: 'HTML5, CSS, Javascript, Perl (backend), Vue.js, Catalyst, Distributed systems, Basalmiq',
+                jobDesc: [
+                    "Introduced and integrated Vue.js library into project front end framework change from Catalyst.",
+                    "Redesigned and drew up wireframes for part of the A3 application to help fix current issues in the displaying new information and features and then develop the features frontend and backend with backend engineers in China."
+                ]
+            },
+            { 
+                company: 'Walt Disney Company: Parks & Resorts', 
+                title: 'Web UI Developer Intern', 
+                date:'Jun 2017 to Jan 2018', 
+                location: 'Orlando, FL',
+                tools: 'SAS, HTML5, CSS, JS, Google Charts API, JQuery, UI/UX wireframing, Python, Basalmiq',
+                jobDesc: [
+                    "Successfully updated/modified the Disney Theatrical System to handle theatre data.",
+                    "Wireframed a new flow for custom query selection for a generated proposal application for using Basalmiq and Powerpoint.",
+                    "Multitasked and redid documentation on multiple projects at a time to develop new client needs and write tests using Python, Javascript"
+                ]
+            }
+        ]
+    }
     render(){
+        const jobList = this.state.jobs.map((job) => 
+            <div className="job">
+                <h3><span className="company">{job.company}</span> / {job.title}</h3>
+                <p className="date">{job.date} || {job.location}</p>
+                <p className="tools">{job.tools}</p>
+                <ul>
+                    {job.jobDesc.map((desc) => 
+                        <li>
+                            {desc}
+                        </li>
+                    )}
+                </ul>
+            </div>
+        )
+        console.log(jobList);
+
         return (
             <section className="resume">
                     <h1>resume.</h1>
@@ -20,63 +87,7 @@ class Resume extends Component{
 
                         <div className="job-list">
                             <h2 className="job"><b>Work Experience</b></h2>
-                            <div className="job">
-                                 <h3>Classic Vacations / Expedia Groups : Web Developer &amp;&amp; UI/UX Engineer</h3>
-                                <p className="date">Jan 2019 to CURRENT</p>
-                                <p className="job-desc">
-                                Javascript , Drupal, CSS3, HTML5, PHP, MySQL, React 
-                                <br />
-                                <ul>
-                                    <li>Created and optimized Javascript and PHP code for different pages and designs for the Classic Vacations site. </li>
-                                    <li>Wireframed and worked with the UX designer and the developer team in India to successfully build a better usable website and optimize the design code. </li>
-                                    <li>Created a style guide with the UX designer to help organize the site's new style and brand.</li>
-                                    <li>Self learned as the only engineer on the team for the first 4 months, a new language and infrastructure, Drupal and php. Introduced React to the team for possibility for changing frameworks to better optimize the site.</li>
-                                    <li>Minimized the amount of libraries imported with CDNs needed for design prototypes into a few lines of Javascript.  Made the prototypes clean and robust enough for use in development. </li>
-                                    <li>Created new documentation for any existing and new code worked on and organized it into documents and onto JIRA boards as the old code didn't have prior documentation to explain certain features.</li>
-                                </ul>
-                                </p>
-                            </div>
-                            <div className="job">
-                                <h3>Aerohive Networks : Full Stack Engineer</h3>
-                                <p className="date">Jan 2018 to Dec 2019</p>
-                                <p className="job-desc">
-                                HTML5, CSS, Javascript, Perl (backend), Vue.js, Catalyst, Distributed systems, Basalmiq
-                                <br />
-                                <ul>
-                                <li>Created frontend features and wireframes for a distributed network cluster system application A3.</li>
-                                <li>Introduced and integrated Vue.js library into project front end framework change from Catalyst.</li>
-                                <li>Communicated with overseas team in China to discuss tasks and complete them</li>
-                                <li>Redesigned and drew up wireframes for part of the A3 application to help fix current issues in the displaying new information and features and then develop the features frontend and backend with backend engineers.</li>
-                                </ul>
-                                </p>
-                            </div>
-                            <div className="job">
-                                <h3>The Walt Disney Company : Web UI Developer Intern</h3>
-                                <p className="date">Jun 2017 to Jan 2018</p>
-                                <p className="job-desc">SAS, HTML5, CSS, JS, Google Charts API, JQuery, UI/UX wireframing, Python, Basalmiq
-                                <br />
-                                <ul>
-                                <li>Successfully updated/modified the Disney Theatrical System with SAS, Javascript, CSS3, Google Charts API, JQuery</li>
-                                <li>Created and wireframed a new UX/UI flow for custom query selection for a generated proposal application for using Basalmiq and Powerpoint</li>
-                                <li>Multitasked and redid documentation on multiple projects at a time to develop new client needs and write tests using Python, Javascript</li>
-                                <li>Modified layouts and code to improve documentation and flow of the website.</li>
-                                <li>Wireframed flow for future query function application.</li>
-                                </ul>
-                                </p>
-                            </div>
-                            <div className="job">
-                                <h3>CISCO : Hardware QA Engineering Intern</h3>
-                                <p className="date">Jun 2016 to Aug 2016</p>
-                                <p className="job-desc">
-                                Python, JS, HTML
-                                <br />
-                                <ul>
-                                <li>Wrote new scripts to help further improve the QA team’s ability to diagnose hardware components in routers/chassis from outside of the lab and to test untested components. Used TMUX, Linux, and TCL to write scripts.</li>
-                                <li>Tested hardware and scripts in the server lab.</li>
-                                <li>Improved the QA website by adding small helpful features such as recognizing who is running a test at a certain time.</li>
-                                </ul>
-                                </p>
-                            </div> 
+                            {jobList}
                         </div>
                     </div>
             </section>
